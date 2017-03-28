0dc7d2a1b764e1687006d3fe4b8ba9b19ba8e59c
Kevin C. Baird <kbaird@users.noreply.github.com>
2017-03-28 09:37:41 -0700

    Add paths to internal mix.exs file

--- apps/sample_app/mix.exs
+++ apps/sample_app/mix.exs
@@ -4,6 +4,10 @@ defmodule SampleApp.Mixfile do
   def project do
     [app:             :sample_app,
      version:         "0.1.0",
+     build_path:      "../../_build",
+     config_path:     "../../config/config.exs",
+     deps_path:       "../../deps",
+     lockfile:        "../../mix.lock",
      elixir:          "~> 1.4",
      elixirc_paths:   elixirc_paths(Mix.env),
      build_embedded:  Mix.env == :prod,
