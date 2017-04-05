30e8346022906baea4609966d458bd49f9d71ac4
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-04-04 19:50:01 -0700

    handle both float and integer operands

--- apps/sample_app_web/lib/sample_app_web/controllers/calculation_controller.ex
+++ apps/sample_app_web/lib/sample_app_web/controllers/calculation_controller.ex
@@ -7,8 +7,17 @@ defmodule SampleApp.Web.CalculationController do
 
   def show(conn, %{"operand"   => operand,
                    "operation" => operation} = _params) do
-    result = Math.calculate({operation, String.to_float(operand)})
+    result = Math.calculate({operation, formatted_operand(operand)})
     send_resp(conn, 200, "#{result}")
+  end
+
+  ### PRIVATE FUNCTIONS
+
+  defp formatted_operand(operand) do
+    case String.match?(operand, ~r/\./) do
+      true -> String.to_float(operand)
+      _    -> String.to_integer(operand)
+    end
   end
 
 end
